7a125e03df32287144d806c9cf410206227c1a3f
Ivan Junoy <ivanjunoy@gmail.com>
2023-09-24 17:44:47 -0300

    pantalla

--- src/index.js
+++ src/index.js
@@ -5,24 +5,36 @@ function random_rgba() {
 
 function createBox() {
     const contenedor = document.querySelector('.contenedor');
-    
     const box = document.createElement('div');
-    box.classList.add('box')
-
+    box.classList.add('box');
     box.style.backgroundColor = random_rgba();
     
-    contenedor.appendChild(box)
+    // Obtener el tamaño del contenedor y el cuadrado
+    const contenedorWidth = window.screen.width
+    const contenedorHeight = window.screen.height
+    const boxSize = 50; // Tamaño del cuadrado (ajusta esto según tu diseño)
+
+    // Calcular cuántos cuadrados caben en el contenedor
+    const cuadradosPorFila = Math.floor(contenedorWidth / boxSize);
+    const cuadradosPorColumna = Math.floor(contenedorHeight / boxSize);
+    const cuadradosTotales = cuadradosPorFila * cuadradosPorColumna;
+    console.log('cuadradosTotales: ', cuadradosTotales);
+    // Obtener todos los cuadrados existentes
+    const cuadradosExistentes = document.querySelectorAll('.box').length;
+
+    // Si no hay más espacio, detener la creación
+    if (cuadradosExistentes >= cuadradosTotales) {
+        clearInterval(intervalo);
+        return;
+    }
+    
+    contenedor.appendChild(box);
 }
 
-
 const intervalo = setInterval(() => {
-    createBox()   
-}, 0)
+    createBox();
+}, 1);
 
-document.body.addEventListener('click', function() {clearInterval(intervalo)});
-
-/* for (let index = 0; index < 1; index++) {
-    setInterval(() => {
-        createBox()   
-    }, 1000)
-} */
+document.body.addEventListener('click', function() {
+    clearInterval(intervalo);
+});
